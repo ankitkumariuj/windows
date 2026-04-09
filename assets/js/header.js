d92f6bccd3325883a8115209bb49c6eb67b791cb
ANKIT KUMAR <ankityadav12044@gmail.com>
2026-04-09 16:04:16 +0530

    update the new code

---
+++ assets/js/header.js
@@ -0,0 +1,34 @@
+window.addEventListener("scroll", function () {
+  let navbar = document.getElementById("navbar");
+
+  if (window.scrollY > 150) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+
+// mobile view menu bar
+
+const openBtn = document.getElementById('openMenu');
+const closeBtn = document.getElementById('closeMenu');
+const menu = document.getElementById('mobileMenu');
+
+// Open Menu
+openBtn.addEventListener('click', () => {
+    menu.classList.add('active');
+    
+});
+
+// Close Menu
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+// Close when clicking outside the sidebar
+window.addEventListener('click', (e) => {
+    if (e.target === menu) {
+        menu.classList.remove('active');
+    }
+});
